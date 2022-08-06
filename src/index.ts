31acb27dd7d72a15c812c746d1d80cabf656267b
Alejandro Sebastian Herrero <110503112+Aleherre14@users.noreply.github.com>
2022-08-06 11:39:31 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+
+var instruccionesTorta = [
+  "prender el horno"
+  "enmantecar la fuente"
+  "mezclar azucar y manteca"
+  "agregar cuatro huevos y batir"
+  "agregarle la harina sernida"
+  "agregarle leche y vainillas"
+  "dejar reposar la masa 30 minutos en la heladera"
+  "ponerle chispitas de chocolate y mezaclar"
+  "agregarle polvo de hornear y escencia de vainilla"
+  "llevar preparacion al horno y dejarlo 1 hora"
+  ]
+
+for (var i = 0; i < instruccionesTorta.length; i++) {
+  console.log("paso:" + [i + 1], instruccionesTorta[i])
+}
+
+
